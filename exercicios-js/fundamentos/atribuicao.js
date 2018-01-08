3fb02d6ce4e72e97e44c615996956b46acd0eba3
Marco Santarelle da Silva Jardim <marco.j@samsung.com>
2018-01-08 17:03:20 -0400

    estudando...2

---
+++ exercicios-js/fundamentos/atribuicao.js
@@ -0,0 +1,19 @@
+const a = 7
+let b = 3
+
+// atribuição aditiva
+b += a // equivalente b = b + a
+console.log(b)
+
+// atribuição subtrativa
+b -= 4 // equivalente b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = b % 2
+console.log(b)
---
+++ exercicios-js/fundamentos/erro.js
@@ -0,0 +1,24 @@
+function tratarErroELancar(erro) {
+    // throw new Error('lançando o erro')
+    // throw 10
+    // throw true
+    // throw 'mensagem'
+    // throw {
+    //  nome: erro.name,
+    //  msg: erro.message,
+    //  date: new Date
+    // }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (erro) {
+        tratarErroELancar(erro)
+    } finally {
+        console.log('final')
+    }
+}
+
+const obj = { nome: 'Roberto' }
+imprimirNomeGritado(obj)
