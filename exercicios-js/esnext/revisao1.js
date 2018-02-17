5376bea93c2688962c3d80f4b974da38256f6064
Marco Jardim <santarelle@gmail.com>
2018-02-16 23:56:31 -0400

    esnext

---
+++ exercicios-js/esnext/revisao1.js
@@ -0,0 +1,24 @@
+// let e const
+{
+    var a = 2
+    let b = 3
+    console.log(b)
+}
+console.log(a)
+
+// Template String
+const produto = 'iPad'
+console.log(`${produto} é caro!`)
+
+// Destructuring
+const [l, e, ...tras] = "Cod3r"
+console.log(l, e, tras)
+
+const [...vector] = "abcde";
+console.log(vector)
+
+const [x, , y] = [1, 2, 3]
+console.log(x, y)
+
+const { idade: i, nome } = { nome: 'Ana', idade: 9 }
+console.log(i, nome)
